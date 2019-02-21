a95d92fcb10344b2091d06881c25ecead57286b7
jun7 <jun7@hush.com>
2019-02-21 23:40:02 +0900

    Remove unnecessary resize for #196

--- src/filer.c
+++ src/filer.c
@@ -934,9 +934,6 @@ gboolean filer_window_delete(GtkWidget *window,
 	if (mount)
 		may_offer_unmount(filer_window, mount);
 
-	/* Without this, the window will flash more */
-	gdk_window_resize(gdkwin(window), 4, 4);
-
 	return FALSE;
 }
 
